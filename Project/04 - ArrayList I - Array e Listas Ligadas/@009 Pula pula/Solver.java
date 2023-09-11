507257f5002124d631e48a9a15415e04980a9db5
Hexcold <henriquepiresh3@gmail.com>
2023-09-11 20:32:29 -0300

    adição da questão 009 Pula pula

--- Project/04 - ArrayList I - Array e Listas Ligadas/@009 Pula pula/Solver.java
+++ Project/04 - ArrayList I - Array e Listas Ligadas/@009 Pula pula/Solver.java
@@ -1,32 +1,54 @@
-import java.util.LinkedList;
+/*
+O que fez?
+    Fiz tudo e passou em todos os testes
+Com quem fez?
+    Fiz sozinho.
+O que aprendeu?
+    Entendi melhor o como realizar de maneira mais específica a adição ou remoção no início e no fim de um LinkedList.
+Quanto tempo levou?
+    1 horas (não sei exatamente, isso é uma especulação )
+*/
+
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Solver {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Trampoline tramp = new Trampoline();
-        while(true) {
+
+        while (true) {
             String line = scanner.nextLine();
-            System.out.println("$"+ line);
+            System.out.println("$" + line);
             String[] ui = line.split(" ");
-            if(ui[0].equals("end")) {
-                break;
-            } else if(ui[0].equals("arrive")) { // name age
-                tramp.arrive(new Kid(ui[1], Integer.parseInt(ui[2]))) ;
-            } else if(ui[0].equals("enter")) {
-                tramp.enter();
-            } else if(ui[0].equals("leave")) {
-                tramp.leave();
-            } else if(ui[0].equals("remoteKid")) {//name
-                tramp.remoteKid(ui[1]);
-            } else if(ui[0].equals("show")) {
-                System.out.println(tramp);
-            } else {
-                System.out.println("fail: comando invalido");
+
+            switch (ui[0]) {
+                case "end":
+                    return;
+
+                case "arrive":
+                    tramp.arrive(new Kid(ui[1], Integer.parseInt(ui[2])));
+                    break;
+
+                case "enter":
+                    tramp.enter();
+                    break;
+
+                case "leave":
+                    tramp.leave();
+                    break;
+
+                case "remove":
+                    tramp.remoteKid(ui[1]);
+                    break;
+
+                case "show":
+                    System.out.println(tramp);
+                    break;
+
+                default:
+                    System.out.println("fail: comando invalido");
+                    break;
             }
         }
-        scanner.close();
     }
 }
-
--- Project/04 - ArrayList I - Array e Listas Ligadas/@009 Pula pula/trampoline.java
+++ Project/04 - ArrayList I - Array e Listas Ligadas/@009 Pula pula/trampoline.java
@@ -11,21 +11,46 @@ public class Trampoline{
     }
 
     private Kid removeFromList(String name, LinkedList<Kid> list) {
-        
+        if(list.size() > 0){
+            list.removeLast();
+            return null;
+        }
+        return null;
     }
 
     public void arrive(Kid kid) {
-        this.waiting.add(kid);
+        this.waiting.addFirst(kid);
     }
 
     public void enter() {
+        if(this.waiting.size() > 0){
+            this.playing.addFirst(this.waiting.removeLast());
+        }
     }
 
     public void leave() {
-    }
+        if(this.playing.size() > 0){
+            this.waiting.addFirst(this.playing.removeLast());
+        }
+    }   
 
     public Kid remoteKid(String name) {
+        for(int i = 0; i < this.playing.size(); i++){
+            if(this.playing.get(i).getName().equals(name)){
+                removeFromList(name, playing);
+                return null;
+            }
+        }
+        for (int i = 0; i < this.waiting.size(); i++) {
+            if (this.waiting.get(i).getName().equals(name)) {
+                removeFromList(name, waiting);
+                return null;
+            }
+        
+        }
+        return null;
     }
+
     public String toString() {
         return   "[" + waiting.stream().map(Kid::toString).collect(Collectors.joining(", ")) + "]" + " => "
                + "[" + playing.stream().map(Kid::toString).collect(Collectors.joining(", ")) + "]";
