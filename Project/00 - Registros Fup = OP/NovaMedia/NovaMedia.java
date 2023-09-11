d30917799a2a0f0d74a7cebd1ad9b01e82240c2e
Hexcold <henriquepiresh3@gmail.com>
2023-09-11 15:05:20 -0300

    Adição dos Makefile

--- Project/00 - Registros Fup = OP/NovaMedia/NovaMedia.java
+++ Project/00 - Registros Fup = OP/NovaMedia/NovaMedia.java
@@ -33,8 +33,8 @@ class Aluno {
     }
 }
 
-class NovaMedia {
-    public static void main(String[] arg) {
+public class NovaMedia {
+    public static void main(String[] args) {
         Aluno alu = new Aluno();
         alu.ler();
         alu.imprimir();
